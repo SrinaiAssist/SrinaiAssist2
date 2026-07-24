851706850298921f4bc5957880cbf8eadd75f3fe
SrinaiAssist <ngadinodiharjo@gmail.com>
2026-07-24 14:17:53 +0700

    Add files via upload

--- android-native/MainActivity.java
+++ android-native/MainActivity.java
@@ -1,7 +1,10 @@
 package com.srinai.assist;
 
 import android.os.Bundle;
-import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -25,14 +28,22 @@ public class MainActivity extends BridgeActivity {
     }
 
     private void hideSystemBars() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
+        // WindowInsetsControllerCompat adalah cara modern & stabil untuk
+        // menyembunyikan status bar + navigation bar (menggantikan
+        // View.SYSTEM_UI_FLAG_* yang deprecated dan tidak reliable lagi
+        // di Android 12+ / edge-to-edge enforcement Android 15).
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        WindowInsetsControllerCompat controller =
+            WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+
+        if (controller != null) {
+            controller.hide(WindowInsetsCompat.Type.systemBars());
+            // Bar akan muncul sementara kalau user swipe dari tepi layar,
+            // lalu otomatis sembunyi lagi (immersive sticky behavior).
+            controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
     }
 }
